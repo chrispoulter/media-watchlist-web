82e828e26ca64e46536ea1da5e0e02a16f01c65c
Chris Poulter <cpoulter@hotmail.co.uk>
2026-05-22 14:05:36 +0100

    refactor: remove VITE_APP_ENVIRONMENT from config and update CI workflow for Azure deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,11 @@ import path from 'node:path';
 
 const gitCommitSha = process.env.GIT_COMMIT_SHA;
 const version = gitCommitSha?.slice(0, 7) ?? process.env.npm_package_version;
-const environment = process.env.NODE_ENV ?? 'development';
 
 // https://vite.dev/config/
 export default defineConfig({
     define: {
         'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
-        'import.meta.env.VITE_APP_ENVIRONMENT': JSON.stringify(environment),
     },
     plugins: [react(), tailwindcss()],
     resolve: {
